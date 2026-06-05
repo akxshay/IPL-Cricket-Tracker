dfa6eff48b8fe75087f0b3a5fb3ef482b25f6790
Akshay Kumar S <155802114+akxshay@users.noreply.github.com>
2026-06-05 19:58:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * This file is listed in .gitignore
  */
 const CONFIG = {
-  API_KEY:        "10197a12f9msh0173929f03dea89p16bb48jsn824900e899e4",
+  API_KEY:        "your_api_key",
   BASE_URL:       "https://cricket-api-free-data.p.rapidapi.com",
   API_HOST:       "cricket-api-free-data.p.rapidapi.com",
   CACHE_DURATION: 60000,
